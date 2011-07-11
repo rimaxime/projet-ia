932d82077a95207114553b0bb3eb95106a44ce86
rimaxime <rimaxime@c22dacbe-49da-11f6-1edc-126d28ae6754>
2011-07-11 17:55:19 +0000

    Création fichiers structures et constantes

---
+++  projet-ia/inc/constantes.h
@@ -0,0 +1,7 @@
+/**
+  * \file constantes.h
+  * \brief Contient l'ensemble des constantes du programme 
+  * utilisés tout au long du code
+  * \author MRI BEC
+  * \date 11/07/2011
+  */
---
+++  projet-ia/inc/structures.h
@@ -0,0 +1,8 @@
+/**
+  * \file structures.h
+  * \brief Contient l'ensemble des structures du programme 
+  * utilisés tout au long du programme
+  * \author MRI BEC
+  * \date 11/07/2011
+  */
+
