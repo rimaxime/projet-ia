7b5b340436ae157aff12035b7acccd2683f87860
rimaxime <rimaxime@c22dacbe-49da-11f6-1edc-126d28ae6754>
2011-07-11 20:15:54 +0000

    Commit des constantes, et du .h du fichier sur lequel je travaille afin que tu ai les constantes à jour et que tu puisses travailler convenablement.

---
+++  projet-ia/inc/gestion_fichiers.h
@@ -0,0 +1,25 @@
+/**
+  * \file gestion_fichiers.h
+  * \brief Récupére les données des bases de données pour les
+  * les mettre dans des tableaux / listes
+  * \author MRI
+  * \date 11/07/2011
+  */
+
+#include "../inc/constantes.h"
+#include "../inc/structures.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+	* \fn int RecupererInfosDepartement(STR_DEPARTEMENT * tableau_departements)
+  * \brief Cette fonction va balayer le fichier de départements afin de récupérer les normales météo mensuelles de 
+  * ses départements.
+  * \param tableau_departements Tableau des départements
+	* \return 0 si pas d'erreur -1 sinon
+  */
+
+int RecupererInfosDepartement(STR_DEPARTEMENT * tableau_departements);
