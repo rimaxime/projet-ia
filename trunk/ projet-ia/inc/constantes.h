349fde99408530b7995c05be075963533b9915be
rimaxime <rimaxime@c22dacbe-49da-11f6-1edc-126d28ae6754>
2011-07-11 20:15:54 +0000

    Commit des constantes, et du .h du fichier sur lequel je travaille afin que tu ai les constantes à jour et que tu puisses travailler convenablement.

--- trunk/ projet-ia/inc/constantes.h
+++ trunk/ projet-ia/inc/constantes.h
@@ -5,3 +5,33 @@
   * \author MRI BEC
   * \date 11/07/2011
   */
+
+/**
+  * \brief Macro-programmation de gestion des erreurs
+  */
+
+#define ERREUR_FICHIER_NON_EXISTANT \
+  fprintf(stderr, " Le fichier demandé n'existe pas - %s (%d)\r\n", __FILE__, __LINE__);
+
+#define ERREUR_FICHIER \
+  fprintf(stderr, " Erreur de lecture de fichier - %s (%d)\r\n", __FILE__, __LINE__);
+
+#define ERREUR_MEMOIRE \
+  fprintf(stderr, " Erreur d'allocation mémoire - %s (%d)\r\n", __FILE__, __LINE__);
+
+
+
+
+
+
+/**
+  * \brief Constantes sur les tailles maximales des chaines de caractéres & tableaux
+  */
+
+
+
+/**
+  * \brief Constantes contenant les chemins/noms des différents fichiers
+  */
+
+
