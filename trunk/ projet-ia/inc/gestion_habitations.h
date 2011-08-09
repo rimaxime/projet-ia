d406bfd29833195bbd1f202179fd1bc601f76400
rimaxime <rimaxime@c22dacbe-49da-11f6-1edc-126d28ae6754>
2011-08-09 14:19:11 +0000

    Modification du makefile pour future prise en compte de GTK+
    Création des fonctions de destruction pour pouvoir libérer la mémoire en fin de programme.
    
    
    Modification d'un prototype & donc du test associé dans gestion habitations (CreerHabitation()).

--- trunk/ projet-ia/inc/gestion_habitations.h
+++ trunk/ projet-ia/inc/gestion_habitations.h
@@ -1,6 +1,16 @@
+/**
+  * \file gestion_habitations.h
+  * \brief Crée les maisons et les différentes pièces associées
+  * \author MRI
+  * \date 08/08/2011
+  */
+
+
 
 #ifndef GESTION_HABITATIONS_H
 #define GESTION_HABITATIONS_H
+
+
 
 //Include
 #include <stdlib.h>
@@ -12,7 +22,7 @@
 #include "../inc/structures.h"
 
 
-ST_HABITATIONS CreationHabitation(ST_HABITATIONS Nouveau,int nombre_pieces, float inclinaison_toit, int Isolation,int Exposition);
+ST_HABITATIONS* CreationHabitation(int nombre_pieces, float inclinaison_toit, int Isolation,int Exposition);
 
 ST_PIECES* CreationPiece(char* nom_piece, int type_piece, float Largeur, float Longueur, float coordX1, float coordY1,float coordX2, float coordY2);
 
@@ -26,5 +36,13 @@ ST_EQUIPEMENTS* InsererTrierEquipements(ST_EQUIPEMENTS *Nouveau, ST_EQUIPEMENTS 
 ST_PANNEAUX* CreationPanneaux(int indice, int type, float Largeur, float Longueur, float inclinaison_panneau, int Exposition);
 
 ST_PANNEAUX* InsererTrierPanneaux(ST_PANNEAUX *Nouveau, ST_PANNEAUX *Tete);
+
+void Detruire_Habitation(ST_HABITATIONS *Habitation);
+
+void Detruire_Pieces(ST_PIECES *Tete);
+
+void Detruire_Equipements(ST_EQUIPEMENTS *Tete);
+
+void Detruire_Panneaux(ST_PANNEAUX *Tete);
 
 #endif
