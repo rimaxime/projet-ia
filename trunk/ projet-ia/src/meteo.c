d5f394416b664ce21ccda2d5e4a21528d8957285
benoit.eche@gmail.com <benoit.eche@gmail.com@c22dacbe-49da-11f6-1edc-126d28ae6754>
2011-08-17 23:05:34 +0000

    Bug dans le calcul de la meteo lorsque l'on est plus dans les limites min et max.
    Lorsque l'on a réécrit la formule du calcul a Rousset. 
    On a mis :
      if(temp>(Normales_Mensuelles.temp_max))	
        temp=Normales_Mensuelles.temp_max;		
      else if(temp<Normales_Mensuelles.temp_min)	
        temp=Normales_Mensuelles.temp_min;
    
    Ce qui fait que lorsque que l'on commence avec une température supérieure  a temp_max ou une inferieure avec temp_min on reste bloqué à temp_min ou temp_max.
    Je vais voir de coder un montée ou descente aléatoirement "lineraire".

--- trunk/ projet-ia/src/meteo.c
+++ trunk/ projet-ia/src/meteo.c
@@ -28,10 +28,10 @@ float SimulationTemp(ST_SimuMeteo Meteo_Precedente,ST_DonneeMeteo Normales_Mensu
   //Calcul de la temérature journalière
   temp = (Normales_Mensuelles.temp_min + ((Normales_Mensuelles.temp_max-Normales_Mensuelles.temp_min)*nombre_aleatoire)) * 0.6 
 	  + Meteo_Precedente.temp * 0.4;
-  if(temp>(Normales_Mensuelles.temp_max))
-    temp=Normales_Mensuelles.temp_max;
-  else if(temp<Normales_Mensuelles.temp_min)
-    temp=Normales_Mensuelles.temp_min;
+  if(temp>(Normales_Mensuelles.temp_max))	// A corriger
+    temp=Normales_Mensuelles.temp_max;		// A corriger
+  else if(temp<Normales_Mensuelles.temp_min)	// A corriger
+    temp=Normales_Mensuelles.temp_min;		// A corriger
   return temp;   
 }
 
