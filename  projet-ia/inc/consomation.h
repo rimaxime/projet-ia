c4d0e501c5925bbdbe4916207ce76789f115a45b
benoit.eche <benoit.eche@gmail.com>
2011-08-31 21:15:25 +0000

    Ajout des fichiers de consomation + mise a jour makefile

---
+++  projet-ia/inc/consomation.h
@@ -0,0 +1,17 @@
+#ifndef PRODUCTION_H
+#define PRODUCTION_H
+
+//Include
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include "constantes.h"
+#include "structures.h"
+
+
+//Prototypes
+
+
+#endif
---
+++  projet-ia/tests/test_consomation.c
@@ -0,0 +1,20 @@
+//Includes
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include "../inc/constantes.h"
+#include "../inc/structures.h"
+#include "../inc/production.h"
+#include "../inc/meteo.h"
+#include "../inc.consomation.h"
+
+//Prototypes
+
+
+//Main
+int main(void)
+{
+  
+  return 0;
+}
