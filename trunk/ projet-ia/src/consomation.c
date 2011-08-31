f6e81b718164fa20d7925db90996619c89dd500f
benoit.eche@gmail.com <benoit.eche@gmail.com@c22dacbe-49da-11f6-1edc-126d28ae6754>
2011-08-31 21:15:25 +0000

    Ajout des fichiers de consomation + mise a jour makefile

---
+++ trunk/ projet-ia/src/consomation.c
@@ -0,0 +1,10 @@
+/**
+  * \file consomation.c
+  * \brief Fichier contenant les fonctions utiles pour le calcul de la consomation electrique du foyer
+  * \author Benoit Eche
+  * \date 31/08/11
+  */
+
+#include "../inc/consomation.h"
+
+//Fonctions
